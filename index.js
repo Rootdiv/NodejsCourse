3bd2999e2bdfd2a8d7abbb894d4f48fd55dba221
Rootdiv <rootdiv@mail.ru>
2023-08-31 22:30:55 +0300

    feat: lesson 7 task 1

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
+import readline from 'node:readline/promises';
 import process from 'node:process';
+import { readFile } from 'node:fs/promises';
 
-const write = str => process.stdout.write(str);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const read = cb => {
-  process.stdin.on('data', chunk => {
-    cb(chunk.toString('utf-8'));
-  });
+const write = str => {
+  process.stdout.write(str);
 };
 
-//const clear = () => write('\x1BC');
+// const clear = () => {
+//   write('\x1BC');
+// };
 //Корректная очистка консоли Ubuntu Mate
-const clear = () => write('printf \x1bc');
+const clear = () => {
+  write('printf \x1bc');
+};
 
-const pos = (row, col) => write(`\x1b[${row};${col}H`);
+let questionCount = 0;
+let answerCount = 0;
 
-const box = (row, col, height, width) => {
-  const border = ['┌', '─', '┐', '│', '└', '┘'];
-  const w = width - 2;
-  const h = height - 2;
-  pos(row, col);
-  write(border[0] + border[1].repeat(w) + border[2]);
-  for (let i = 1; i < h; i++) {
-    pos(row + i, col);
-    write(border[3] + ' '.repeat(w) + border[3]);
+const data = await readFile('./question.json');
+const questions = JSON.parse(data.toString('utf-8'));
+
+const endQuiz = answerCount => {
+  write('\nОпрос закончен.\n');
+  write(`Вы правильно ответили на ${answerCount} вопросов\n`);
+  rl.close();
+};
+
+const quiz = async () => {
+  const question = questions[questionCount];
+  write(`\n${question.question}`);
+
+  const options = question.options;
+  for (let i = 0; i <= options.length; i++) {
+    write(`\n${i + 1}. ${options[i]}`);
   }
-  pos(row + h, col);
-  write(border[4] + border[1].repeat(w) + border[5]);
+
+  const answer = await rl.question('Ваш ответ: ');
+
+  if (+answer === 0) {
+    endQuiz(answerCount);
+    return;
+  }
+  if (isNaN(+answer) || +answer < 0 || +answer > options.length) {
+    write('Ответ некорректный! Попробуйте еще раз.\n');
+    quiz(question);
+  } else {
+    questionCount++;
+    if (+answer === question.correctIndex + 1) {
+      write('Правильный ответ!\n');
+      answerCount++;
+      quiz(question);
+    } else {
+      write('Неправильный ответ.\n');
+      quiz(question);
+    }
+  }
+  if (questionCount >= 18) {
+    endQuiz(answerCount);
+    return;
+  }
 };
 
 clear();
+write(`Ответьте на ${questions.length} вопросов по Node.js и JavaScript,`);
+write('\nдля выбора варианта ответа введите его номер.\n');
+write('Нажмите Enter, чтобы начать');
 
-box(4, 4, 4, 30);
+rl.once('line', () => {
+  quiz();
+});
 
-pos(5, 5);
-
-write('Имя: ');
-
-read(str => {
-  write(`\nПривет, ${str.trim()}!\n`);
+rl.on('close', () => {
   process.exit();
 });
---
+++ lesson.js
@@ -0,0 +1,50 @@
+import process from 'node:process';
+
+const write = str => {
+  process.stdout.write(str);
+};
+
+const read = cb => {
+  process.stdin.on('data', chunk => {
+    cb(chunk.toString('utf-8'));
+  });
+};
+
+// const clear = () => {
+//   write('\x1BC');
+// };
+//Корректная очистка консоли Ubuntu Mate
+const clear = () => {
+  write('printf \x1bc');
+};
+
+const pos = (row, col) => {
+  write(`\x1b[${row};${col}H`);
+};
+
+const box = (row, col, height, width) => {
+  const border = ['┌', '─', '┐', '│', '└', '┘'];
+  const w = width - 2;
+  const h = height - 2;
+  pos(row, col);
+  write(border[0] + border[1].repeat(w) + border[2]);
+  for (let i = 1; i < h; i++) {
+    pos(row + i, col);
+    write(border[3] + ' '.repeat(w) + border[3]);
+  }
+  pos(row + h, col);
+  write(border[4] + border[1].repeat(w) + border[5]);
+};
+
+clear();
+
+box(4, 4, 4, 30);
+
+pos(5, 5);
+
+write('Имя: ');
+
+read(str => {
+  write(`\nПривет, ${str.trim()}!\n`);
+  process.exit();
+});
