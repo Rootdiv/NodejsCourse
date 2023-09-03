cd8c6a85389374cde7fa425334005d33538f58c8
Rootdiv <rootdiv@mail.ru>
2023-09-03 21:58:44 +0300

    fix: fix adding id to todo

--- service/addTask.services.js
+++ service/addTask.services.js
@@ -3,7 +3,8 @@ import { writeTodo } from '../modules/writeTodo.js';
 
 export const addTask = async task => {
   const todoList = JSON.parse((await readTodo('./todo.json')) || '[]');
-  const id = todoList.length + 1;
+  const lastId = todoList[todoList.length - 1]?.id;
+  const id = lastId ? lastId + 1 : 1;
   const newTask = { id, status: 'В работе', task };
   writeTodo('./todo.json', JSON.stringify([...todoList, newTask]));
   console.log(`Задача добавлена с идентификатором ${id}`);
