c539255ef7bf85a67ea5f872cd106cb37b227cb1
Rootdiv <rootdiv@mail.ru>
2023-09-08 21:04:35 +0300

    feat: lesson11

--- modules/readData.js
+++
@@ -1,6 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-export const readData = async pathFile => {
-  const data = await readFile(pathFile, 'utf-8');
-  return JSON.parse(data);
-};
--- modules/writeData.js
+++
@@ -1,6 +0,0 @@
-import { writeFile } from 'node:fs/promises';
-
-export const writeData = async (res, pathFile, data, code) => {
-  await writeFile(pathFile, JSON.stringify(data), 'utf-8');
-  res.statusCode = code;
-};
