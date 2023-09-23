a6a517b7e51f4ff45f4d03f6abb65b108d02e66d
Rootdiv <rootdiv@mail.ru>
2023-09-23 11:56:47 +0300

    fix: maximum discount value

--- modules/formatNumData.js
+++ modules/formatNumData.js
@@ -1,5 +1,5 @@
 export const formatNumData = data => {
-  const discount = parseInt(data.discount) >= 0 ? parseInt(data.discount) : 0;
+  const discount = parseInt(data.discount) >= 0 && parseInt(data.discount) < 100 ? parseInt(data.discount) : 0;
   const price = parseFloat(data.price) > 0 ? parseFloat(data.price) : 0;
   const count = parseInt(data.count) > 0 ? parseInt(data.count) : 0;
 
